e3057be31261c1b0e849d55d793a9c51a21e880c
itsahadul99 <itsahadul999@gmail.com>
2025-03-16 17:23:58 +0600

    initialize project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./*.html", "./sections/**/*.liquid", "./templates/**/*.liquid"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
